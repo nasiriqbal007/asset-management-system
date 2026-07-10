f800b20cd5263d21c7bcb0bfc01fee2a75ae3b03
nasiriqbal007 <nasiriqbal1198@gmail.com>
2026-07-10 19:10:41 +0500

    fix: update baseURL to use VITE_API_URL in axios instance

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASEURL,
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 5000,
 });
 api.interceptors.request.use(function (config) {
